57ef683595d253c9849c0e83bd24c82fb8c40ad9
Чёрный Ярослав <pirotexnic27@gmail.com>
2021-07-14 23:34:21 +0500

    Revert "little refresh"
    This reverts commit 7dbc58c4b9662163da316cf38bafac0b0ac4c647.

--- FFMPEG_Overlay/Program.cs
+++ FFMPEG_Overlay/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace FFMPEG_Overlay
 {
-    static class StartPoint
+    static class Program
     {
         /// <summary>
         /// Главная точка входа для приложения.
